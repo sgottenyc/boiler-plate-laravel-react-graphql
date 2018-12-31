f8d43f8b497f506c87d2eb2079f34b37cd0d038b
jiunjye704 <jiunjye704@gmail.com>
2018-12-31 16:38:06 -0500

    added if app is prod then use ssl

---
+++ resources/js/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import {routerReducer} from 'react-router-redux';
+
+export default function rootReducer(client) {
+  const myRootReducer = combineReducers({
+  routing: routerReducer,
+  apollo: client.reducer()
+  });
+  return myRootReducer;
+}
