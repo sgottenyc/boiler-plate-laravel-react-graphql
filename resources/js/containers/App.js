7f7ec759d8b5ee5a5c80292378132995e8bdef9e
Jiun Chin <jiun.chin@aecom.com>
2018-12-31 14:22:51 -0500

    added containers

--- resources/js/containers/App.js
+++ resources/js/containers/App.js
@@ -4,15 +4,24 @@ import ApolloClient from "apollo-boost";
 import LoginPage from '../containers/LoginPage';
 import { ApolloProvider } from "react-apollo";
 import AppRouter from '../routes/appRouter';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import appReducer from '../reducers/appReducer';
 
 const client = new ApolloClient({
     uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
-  });
+  }); 
+
+const store = createStore(
+    combineReducers({
+      app: appReducer
+    }),
+    {}, // initial state
+);
   
 class App extends Component {
   render () {
     return (
-      <ApolloProvider client={client}>
+      <ApolloProvider client={client} store={store}>
         <AppRouter/>
       </ApolloProvider>
     )
